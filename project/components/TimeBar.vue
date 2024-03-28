4e18d4b20c91411838fe180e329f28eb216fa4a9
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-03-28 12:05:17 -0600

    add content

---
+++ project/components/TimeBar.vue
@@ -0,0 +1,21 @@
+<script>
+  import dayjs from 'dayjs';
+  export default {
+    data() {
+      return {
+        currentDate: '',
+        currentTime: ''
+      };
+    },
+    mounted() {
+      this.currentDate = dayjs().format('YYYY-MM-DD');
+      this.currentTime = dayjs().format('HH:mm:ss');
+    }
+  };
+</script>
+<template>
+  <header>
+    <div v-bind="date" class="font-sans">{{ currentDate }}</div>
+    <div v-bind="time" class="font-sans">{{ currentTime }}</div>
+  </header>
+</template>
