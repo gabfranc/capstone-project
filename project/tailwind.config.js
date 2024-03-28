0e83f56adcbcc99beda63851e430d267f92d236e
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-03-28 11:55:42 -0600

    installed dayjs

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [    
+    "./components/**/*.{js,ts,vue}",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts,vue}",
+    "./app.vue",
+  ],
   theme: {
+    fontFamily: {
+      sans: ['Jost', 'sans-serif'],
+      serif: ['Jomhuria', 'serif']
+    },
     extend: {},
   },
   plugins: [],
